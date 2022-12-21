18e7571fb14283f71ada3479afddffffdcbc06fb
adil <79858381+Adilet-Sen@users.noreply.github.com>
2022-12-21 14:28:22 +0600

    Add half quetions

--- main.py
+++ main.py
@@ -23,3 +23,41 @@ some_phonc(1,2, text="text")
 def sum_arg(a, b): return a + b
 # аналог 
 sum_arg = lambda a, b: a + b
+
+# 8
+import copy
+
+# original object
+original = [[1, 2], [3, 4]]
+
+# shallow copy
+shallow_copy = copy.copy(original)
+
+# deep copy
+deep_copy = copy.deepcopy(original)
+
+# modify the original object
+original[0][0] = 9
+
+print("Original:", original, id(original))
+print("Shallow copy:", shallow_copy, id(shallow_copy))
+print("Deep copy:", deep_copy, id(deep_copy))
+
+#Original: [[9, 2], [3, 4]]
+#Shallow copy: [[9, 2], [3, 4]]
+#Deep copy: [[1, 2], [3, 4]]
+
+class MyClass:
+    count = 0
+    value = 0
+    def __init__(self, value):
+        self.value = value
+        MyClass.count += 1
+
+    @classmethod
+    def get_count(cls):
+        return cls.count + cls.value
+
+obj1 = MyClass(10)
+obj2 = MyClass(20)
+print(MyClass.get_count()) 
